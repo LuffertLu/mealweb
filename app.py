38adf5c5e18a14c89f91003cbef5c6efbdfcda9c
luffert <luffert@163.com>
2019-05-30 09:14:04 +0000

    update app.py

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 from flask import Flask
+import recommend
 app=Flask(__name__)
 @app.route('/')
 def index():
-    return "hello"
+    return render_template('Index.html')
+
+@app.route('/search/')
+def search():
+    n=request.args.get('user')
+    dic=recommend.recommend(n)
+    return render_template('search.html',Data=dic)
+
 if __name__=="__main__":
     app.run(debug=True)
