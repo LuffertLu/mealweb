d347f755ab2d915111e73768fc1cab55c3dad317
luffert <luffert@163.com>
2019-10-24 06:36:05 +0000

    stucture and dependcy save

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,21 @@
+#Internal dependency
+from app.main import main as main_blueprint
+from config import config
+
+#Flask related dependency
 from flask import Flask,render_template,request 
 from flask_bootstrap import Bootstrap
 from flask_mail import Mail
 from flask_moment import Moment
-from flask_sqlalchemy import SQLAlchemy
-from flask.ext.mail import Mail
-from app.main import main as main_blueprint
-from config import config
 
+#Database related dependency
+from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, backref
+from flask_sqlalchemy import SQLAlchemy
+
+#Initialization start
 bootstrap = Bootstrap()
 mail = Mail()
 moment = Moment()
@@ -23,6 +32,4 @@ def create_app():
 	db.init_app(app)
 
 	return app
-
-
 
--- app/models.py
+++ app/models.py
@@ -1,20 +1,18 @@
 from . import db
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 
 engine = create_engine('mysql://root@localhost/study?charset=utf8')
 Base = declarative_base(engine)
-
+Base.metadata.create_all()
 
 
 class Role(db.Model):
 	__tablename__ = 'roles'
 	id = db.Column(db.Integer, primary_key = True)
-	name = db.Column(db.String(64), unique = True)
+	name = db.Column(db.String(64), unique = True, nullable = False)
 	users = db.relationship('User', backref = 'role', lazy = 'dynamic')
 
 	def __repr__(self):
-		return '<Role %r>' % self.name
+		return '<Role: %r>' % self.name
 
 
 
@@ -22,9 +20,32 @@ class User(db.Model):
 	"""docstring for User"""
 	__tablename__ = 'users'
 	id = db.Column(db.Integer, primary_key = True)
-	username = db.Column(db.String(64), unique = True, index = True)
+	username = db.Column(db.String(64), unique = True, index = True, nullable = False)
 	role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
 	def __repr__(self):
-		return '<User %r>' % self.username
-		
+		return '<User: %r>' % self.username
+
+
+
+class Student(Base):
+	"""docstring for Student"""
+	__tablename__ = 'student'
+	id = Column(Integer, primary_key = True)
+	name = Column(String(64), unique = True, nullable = False)
+	email = Column(String(64), unique = True)
+	
+	def __repr__(self):
+		return '<Student: %r>' % self.name
+
+
+
+class Course(Base):
+	"""docstring for Course"""
+	__tablename__ = 'course'
+	name = Column(String(64))
+	student_id = Column(Integer, ForeignKey('student.id', ondelete = 'CASCADE'))	
+	student = relationship('Student', backref = backref('course', cascade = 'all, delete-orphan'))
+
+	def __repr__(self):
+		return '<Course: %r>' % self.name		
