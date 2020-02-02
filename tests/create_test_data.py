c2f7371c9ff74d2557921c92739bb5ca4afc3549
luffert <luffert@163.com>
2020-02-02 13:14:17 +0800

    add suggestion template

--- tests/create_test_data.py
+++ tests/create_test_data.py
@@ -1,6 +1,10 @@
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
+from sqlalchemy.orm import sessionmaker
+from faker import Faker
+from db import engine, User, Course, Lab, Tag
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative imort declarative_base
 from ..app.models import Plant, Meat
@@ -35,5 +39,38 @@ def main():
 	create_plant()
 	session.commit()
 
+
+
+session = sessionmaker(engine)()
+fake = Faker('zh-cn')
+
+def create_users():
+    for i in range(10):
+        user = User(name=fake.name(), email=fake.email())
+        session.add(user)
+
+def create_courses():
+    for user in session.query(User).all():
+        for i in range(2):
+            course = Course(name=''.join(fake.words(4)), user=user)
+            session.add(course)
+
+def create_labs():
+    for course in session.query(Course):
+        lab = Lab(name=''.join(fake.words(5)), id=course.id)
+        session.add(lab)
+
+def create_tags():
+    for name in ['python', 'linux', 'java', 'mysql', 'lisp']:
+        tag = Tag(name=name)
+        session.add(tag)
+
+def main():
+    create_users()
+    create_courses()
+    create_labs()
+    create_tags()
+    session.commit()
+
 if __name__ == '__main__':
-	main()
+    main()
